2f50d3762a5846f8a9799bf36330a0c45c306501
jirlinge <julienirlinger@gmail.com>
2021-02-16 14:55:10 +0100

    premiere version

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            bool rainy = true;
+
+            if (rainy == true)
+            {
+                Console.WriteLine("Bonjour il ne pleut pas aujourd'hui bonne journée");
+            }
+
+            else
+            {
+                Console.WriteLine("il va pleuvoir, vous devriez prendre un parapluie");
+            }
+        }
+    }
+}
